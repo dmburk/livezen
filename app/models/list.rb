2ed07dc80bb2a33fd2204d0a6a9bbdd25089615d
Michael Burk <michael.burk@gmail.com>
2011-11-18 19:52:04 -0500

    Added more list model validations.

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,5 +2,7 @@ class List < ActiveRecord::Base
 	has_many :list_shares
 	has_many :users, :through => :list_shares
 
+	attr_accessible :name
+
 	validates :name, :presence => true
 end
--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -1,8 +1,11 @@
 require 'spec_helper'
 
 describe List do
-	it { should have_many (:list_shares) }
+	it { should have_many(:list_shares) }
 	it { should have_many(:users).through(:list_shares) }
+
+	it { should allow_mass_assignment_of(:name) }
+	it { should_not allow_mass_assignment_of(:id) }
 
 	it { should validate_presence_of :name }
 end
